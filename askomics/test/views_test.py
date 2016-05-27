57d9f6429127721b3dc63fe9efe1765ee4056f25
Maël Kerbiriou <piezo.w.dimd@gmail.com>
2016-05-27 11:56:06 +0200

    Factorize tests setUp and tearDown in AskoTestCase base class.

--- askomics/test/views_test.py
+++ askomics/test/views_test.py
@@ -1,23 +1,16 @@
-import unittest
-
-from pyramid import testing
-from pyramid.paster import get_appsettings
+from askomics.test import AskoTestCase
 from askomics import main
 
 
-class ViewTests(unittest.TestCase):
+class ViewTests(AskoTestCase):
     def setUp(self):
         from askomics.ask_view import AskView
 
-        self.config = testing.setUp()
-        self.settings = get_appsettings('configs/development.ini', name='main')
+        super().setUp()
         self.app = main(self.settings)
 
         from webtest import TestApp
         self.testapp = TestApp(self.app)
-
-    def tearDown(self):
-        testing.tearDown()
 
     def test_home(self):
 
