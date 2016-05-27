cd9df8ed5bca60ca9b9a8633b2850f12a7971cf4
Maël Kerbiriou <piezo.w.dimd@gmail.com>
2016-05-27 11:02:38 +0200

    Travis and coveralls setup for 3 python versions.

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ requires = [
     'pyramid_chameleon',
     'pyramid_debugtoolbar',
     'waitress',
+    'SPARQLWrapper',
     ]
 
 setup(name='Askomics',
@@ -25,15 +26,15 @@ setup(name='Askomics',
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
         ],
-      author='',
-      author_email='',
+      maintainer='Olivier Filangi',
+      maintainer_email='olivier.filangi@rennes.inra.fr',
       url='http://bipaa.genouest.org/',
       keywords='web pyramid pylons',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
       install_requires=requires,
-      tests_require=requires,
+      tests_require=['webtest'],
       test_suite="askomics",
       entry_points="""\
       [paste.app_factory]
