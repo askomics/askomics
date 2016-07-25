761d9310d3942e03b92c7668c79418c2911b6d15
Olivier Filangi <olivier.filangi@rennes.inra.fr>
2016-07-25 08:02:58 +0200

    save current state to generate coverage file from js

--- askomics/test/client/js/GraphNode.js
+++ askomics/test/client/js/GraphNode.js
@@ -1,7 +1,10 @@
+//require('./istanbul/askomics.js');
+
 // tests.js
 describe('GraphNode', function(){
   describe('#Constructeur empty', function(){
     it('* test all methods *', function(){
+      /*
       var node = new GraphNode();
       chai.assert.typeOf(node.SPARQLid,'string');
       chai.assert.isNotOk(node.actif);
@@ -9,10 +12,11 @@ describe('GraphNode', function(){
       chai.assert.typeOf(node.getOpacity(),'string');
       chai.assert.typeOf(node.getNodeStrokeColor(),'string');
       chai.assert.typeOf(node.getColorInstanciatedNode(),'string');
-      /*
+      *//*
       chai.assert.equal(-1, [1,2,3].indexOf(5));
       chai.assert.equal(-1, [1,2,3].indexOf(0));
       */
     });
   });
+//});
 });
