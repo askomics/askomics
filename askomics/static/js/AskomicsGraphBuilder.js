824dadc293b9d9af84e4ca3b852f9a14dc675821
Olivier Filangi <olivier.filangi@rennes.inra.fr>
2016-09-08 14:27:13 +0200

    get a sublist of nodes instancied

--- askomics/static/js/AskomicsGraphBuilder.js
+++ askomics/static/js/AskomicsGraphBuilder.js
@@ -23,8 +23,23 @@ const classesMapping = {
       this._instanciedLinkGraph = [] ;
     }
 
-    nodes() {
-      return this._instanciedNodeGraph;
+    /* get node */
+    nodes(selectedOrderList,kindparam) {
+
+      if ( selectedOrderList === undefined )
+        return this._instanciedNodeGraph;
+
+      let nodeL = [];
+      for (let i in selectedOrderList ) {
+          for (let j in this._instanciedNodeGraph) {
+            console.log(this._instanciedNodeGraph[j][kindparam]);
+            if ( selectedOrderList[i] == this._instanciedNodeGraph[j][kindparam]) {
+              nodeL.push(this._instanciedNodeGraph[j]);
+              break;
+            }
+          }
+        }
+        return nodeL;
     }
 
     links() {
