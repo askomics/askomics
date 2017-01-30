bc5480077d9ab90cbb304c467fef0576f95355ed
Olivier Filangi <olivier.filangi@rennes.inra.fr>
2017-01-30 14:20:53 +0100

    add comments

--- launchAskomics-dev.sh
+++ launchAskomics-dev.sh
@@ -5,8 +5,15 @@ $VENV/bin/easy_install "SPARQLWrapper"
 $VENV/bin/easy_install "Pygments==2.1.3"
 $VENV/bin/easy_install "requests==2.10.0"
 
-#$VENV/bin/python setup.py develop
+#1)npm install
+#2)gulp build
+#3)
+$VENV/bin/python setup.py develop
+#4)
 $VENV/bin/pserve configs/development.virtuoso.ini
+
+
+#Testing others TPS
 #$VENV/bin/pserve configs/development.fuseki.ini
 #$VENV/bin/pserve configs/development.corese.ini
 
