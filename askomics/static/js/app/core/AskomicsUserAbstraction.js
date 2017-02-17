b01a95810be1b958cbcc0e94e8a462c1b3110405
Olivier Filangi <olivier.filangi@rennes.inra.fr>
2017-02-17 10:01:31 +0100

    add modal waiting load of abstraction

--- askomics/static/js/app/core/AskomicsUserAbstraction.js
+++ askomics/static/js/app/core/AskomicsUserAbstraction.js
@@ -128,8 +128,10 @@ class AskomicsUserAbstraction {
 
       let instanceUserAbstraction = this;
 
-      service.postsync(data,function(resultListTripletSubjectRelationObject ) {
+      __ihm.displayModal('Loading abstraction...', '', 'Close');
 
+      service.postsync(data,function(resultListTripletSubjectRelationObject ) {
+      __ihm.hideModal();
       /* All relation are stored in tripletSubjectRelationObject */
       instanceUserAbstraction.tripletSubjectRelationObject = resultListTripletSubjectRelationObject.relations;
       /* == External Service can add external relation == */
