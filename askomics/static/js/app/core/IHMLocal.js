6145ca5ed05c9ee228450f0de337717c6f097f6b
Olivier Filangi <olivier.filangi@rennes.inra.fr>
2017-04-26 16:25:08 +0200

    manage relative libnk to delete file

--- askomics/static/js/app/core/IHMLocal.js
+++ askomics/static/js/app/core/IHMLocal.js
@@ -549,16 +549,6 @@ class IHMLocal {
 
         formHtmlforUploadFiles.html = formHtmlforUploadFiles.html.replace("___TITLE_UPLOAD___",titleForm);
         formHtmlforUploadFiles.html = formHtmlforUploadFiles.html.replace("___SIZE_UPLOAD____",(sizeFileMax/(1000*1000))+" Mo");
-        let urlbase = $(location).attr('href').trim();
-        //console.log(urlbase);
-        //console.log(urlbase.charAt(urlbase.length-1));
-        if ( urlbase.charAt(urlbase.length-1) == "#" || urlbase.charAt(urlbase.length-1) == "/" ) {
-          urlbase = urlbase.substring(0, urlbase.length-2);
-        } 
-        formHtmlforUploadFiles.html = formHtmlforUploadFiles.html.replace('___URL___', urlbase);
-        
-        //console.log(JSON.stringify(location));
-
         $(content).html(formHtmlforUploadFiles.html);
         /*
 
