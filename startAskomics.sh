5e745eec77362e143843bec017cb57b3088772ca
Olivier Filangi <olivier.filangi@rennes.inra.fr>
2017-08-16 17:18:18 +0200

    remove binary test

--- startAskomics.sh
+++ startAskomics.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
 
-# check binaries dependancies
-npm -v >/dev/null 2>&1 || { echo "npm is required but it's not installed.  Aborting." >&2; exit 1; }
-python3 -h >/dev/null 2>&1 || { echo "python3 is required but it's not installed.  Aborting." >&2; exit 1; }
-
 dir_askomics=$(dirname "$0")
 dir_config="$dir_askomics/configs"
 dir_venv="$dir_askomics/venv"
@@ -90,6 +86,8 @@ fi
 
 if [[ ! -d $dir_node_modules ]]; then
     echo "javascript dependancies : npm install"
+    # check binaries dependancies
+    npm -v >/dev/null 2>&1 || { echo "npm is required but it's not installed.  Aborting." >&2; exit 1; }
     npm install
 fi
 
