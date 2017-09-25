4fbf164689a50eabb33554602b2f1e34e4b77ade
Olivier Filangi <olivier.filangi@rennes.inra.fr>
2017-09-25 15:02:13 +0200

    fix minor bugs

--- askomics/test/askView_test.py
+++ askomics/test/askView_test.py
@@ -394,7 +394,6 @@ class AskViewTests(unittest.TestCase):
 
         data = self.askview.get_value()
 
-        print(data)
         assert data == {
             'values': [{
                 'People1': 'p1'
@@ -520,11 +519,12 @@ class AskViewTests(unittest.TestCase):
         """Test get_users_infos"""
 
         self.tps.clean_up()
-
-        data = self.askview.get_users_infos()
-
         # first test with non admin
-        assert data == 'forbidden'
+        try :
+            data = self.askview.get_users_infos()
+            assert False
+        except Exception as e:
+            assert True
 
         # then, is user is admin
         self.request.session['admin'] = True
@@ -557,10 +557,13 @@ class AskViewTests(unittest.TestCase):
             'lock': True
         }
 
-        data = self.askview.lock_user()
-
         # first test with non admin
-        assert data == 'forbidden'
+
+        try:
+            data = self.askview.lock_user()
+            assert False
+        except Exception as e:
+            assert True
 
         # then, is user is admin
         self.request.session['admin'] = True
@@ -582,10 +585,9 @@ class AskViewTests(unittest.TestCase):
 
         try:
             data = self.askview.set_admin()
-            assert False
-        except Exception e:
+        except Exception as e:
             assert True
-            
+
         # then, is user is admin
         self.request.session['admin'] = True
 
@@ -616,6 +618,8 @@ class AskViewTests(unittest.TestCase):
         }
 
         self.request.session['blocked'] = False
+        self.request.session['admin'] = False
+        self.request.session['username'] = 'jdoe'
 
         data = self.askview.delete_user()
 
