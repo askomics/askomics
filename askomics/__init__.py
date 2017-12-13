6f78948669e67165e207b0f256a0e4002d617eab
ofilangi <olivier.filangi@inra.fr>
2017-12-13 18:07:05 +0100

    change timeout 1200->1800

--- askomics/__init__.py
+++ askomics/__init__.py
@@ -8,7 +8,7 @@ from pyramid.session import SignedCookieSessionFactory
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    my_session_factory = SignedCookieSessionFactory('itsaseekreet',timeout=2400)
+    my_session_factory = SignedCookieSessionFactory('itsaseekreet',timeout=1800)
 
     config = Configurator(settings=settings)
     config.set_session_factory(my_session_factory)
