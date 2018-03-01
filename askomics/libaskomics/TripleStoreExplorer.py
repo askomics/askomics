e54727604f8e06bf3f46ddb875f28a12be4bf0c3
ofilangi <olivier.filangi@inra.fr>
2018-03-01 15:38:16 +0100

    change uri askomics prefix for ttl

--- askomics/libaskomics/TripleStoreExplorer.py
+++ askomics/libaskomics/TripleStoreExplorer.py
@@ -70,7 +70,8 @@ class TripleStoreExplorer(ParamManager):
                     private = False
 
                 nodes.append({'endpoint' : endpoint,'g': g, 'uri': uri, 'label': label, 'public': public, 'private': private})
-
+        print("=============================================")
+        print(nodes)
         return nodes
 
     def getUserAbstraction(self):
