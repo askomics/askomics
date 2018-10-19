3553c2e15425a7b16c7a25e9002382220c17d5e8
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2018-10-19 17:36:21 +0200

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -4,6 +4,7 @@ import sys
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'askomics'))
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+master_doc = 'index'
 
 
 def run_apidoc(_):
