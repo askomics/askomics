bb97b090b9258c2292c1b0b23c8fa7b6291a7892
xgaia <xgaia@gmx.com>
2019-01-14 11:24:09 +0100

    typo

--- askomics/views.py
+++ askomics/views.py
@@ -5,6 +5,6 @@ def my_view(request):
 
     return {
         'project': 'AskOmics',
-        'signup': request.registry.settings['askomics.enable_account_creation']  if 'askomics.ldap.enable_account_creation' in request.registry.settings else 'true',
+        'signup': request.registry.settings['askomics.enable_account_creation']  if 'askomics.enable_account_creation' in request.registry.settings else 'true',
         'login_message': request.registry.settings['askomics.ldap.message'] if 'askomics.ldap.message' in request.registry.settings else ''
     }
